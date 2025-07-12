0cfbd98d9749398d61057ada1552a5c028197250
Hussains3 <hussainasabbir3@gmail.com>
2025-07-12 14:56:45 +0600

    Order by order date asc

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -63,7 +63,7 @@ class CustomerController extends Controller
             $to_date = $request->input('to_date');
             $orders = $customer->orders()
                 ->whereBetween('order_date', [$from_date, $to_date])
-                ->orderBy('order_date', 'DESC')
+                ->orderBy('order_date', 'ASC')
                 ->get();
 
             // Get the balance() of last of $orders
@@ -79,7 +79,7 @@ class CustomerController extends Controller
         } else {
             $from_date = null;
             $to_date = null;
-            $orders = $customer->orders()->orderBy('order_date', 'DESC')->get();
+            $orders = $customer->orders()->orderBy('order_date', 'ASC')->get();
 
             $footer_total_bill = $customer->totalbill();
             $footer_paid_bill = $customer->paidbill();
